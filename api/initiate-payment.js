2891bbeeefc5922841fefc61c67fc39b1760e952
mathewkippss-droid <mathewkippss@gmail.com>
2026-04-05 03:36:27 +0300

    Update initiate-payment.js

--- api/initiate-payment.js
+++ api/initiate-payment.js
@@ -26,21 +26,24 @@ export default async function handler(req, res) {
     ]);
 
     // ✅ PAYHERO REQUEST
-const response = await fetch("https://backend.payhero.co.ke/api/v2/payments", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": "Basic QWpBeXNOMFpSWDZIalBBTVVXb206UkNmczh0UkN1RmRZTFdMdFBaaHU0UlkxQjVEODQ0ZWNqeHgzaml4WQ=="
-  },
-  body: JSON.stringify({
-    amount,
-    phone_number: phone,
-    channel_id: 5284,
-    provider: "m-pesa",
-    external_reference: reference,
-    callback_url: "https://fulizaincrease-iota.vercel.app/api/callback"
-  })
-});
+    const AUTH_TOKEN = "QWpBeXNOMFpSWDZIalBBTVVXb206UkNmczh0UkN1RmRZTFdMdFBaaHU0UlkxQjVEODQ0ZWNqeHgzaml4WQ==";
+
+    const response = await fetch("https://backend.payhero.co.ke/api/v2/payments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Basic ${AUTH_TOKEN}`
+      },
+      body: JSON.stringify({
+        amount: amount,
+        phone_number: phone_number,
+        channel_id: 5284,
+        provider: "m-pesa",
+        external_reference: "INV-" + Date.now(),
+        customer_name: "Test User",
+        callback_url: "https://fulizaincrease-iota.vercel.app/api/callback"
+      })
+    });
 
     const data = await response.json();
 
